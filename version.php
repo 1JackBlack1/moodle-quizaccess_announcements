538e88160c59dc31a55596fd947b32caaa655ea3
PraiseSatan <blackjeffrey1@gmail.com>
2023-07-04 14:42:43 +1000

    Update version number after changes

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version = 2023050701;
+$plugin->version = 2023050704;
 $plugin->requires = 2022041200;
 $plugin->component = 'quizaccess_announcements';
-$plugin->release = 'v1.0.0';
+$plugin->release = 'v1.0.1';
